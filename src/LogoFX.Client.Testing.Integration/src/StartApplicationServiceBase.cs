35841a6cd0c6138d51ea254c7b18d3a4db11cd49
Gennady Verdel <godrose@gmail.com>
2018-09-14 00:20:59 +0300

    Renamed RegisterFakes to Setup to reflect the new meaning,

--- src/LogoFX.Client.Testing.Integration/src/StartApplicationServiceBase.cs
+++ src/LogoFX.Client.Testing.Integration/src/StartApplicationServiceBase.cs
@@ -15,20 +15,21 @@ namespace LogoFX.Client.Testing.Integration
         /// <param name="startupPath">The startup path. In the integration tests case, it may be left empty.</param>
         public void StartApplication(string startupPath)
         {
-            RegisterFakes();
+            Setup();
             OnStartCore();
             OnStart(ScenarioHelper.RootObject);
         }
 
         /// <summary>
-        /// Override this method to register fakes into the container before the application starts.
+        /// Override this method to perform setup functionality before the application starts.
         /// </summary>
-        protected virtual void RegisterFakes()
+        protected virtual void Setup()
         {            
         }
 
         /// <summary>
-        /// Override this method to inject custom logic with regard to the application root object immediately after the object has been created.
+        /// Override this method to inject custom logic with regard to the application root object 
+        /// immediately after the object has been created.
         /// </summary>
         /// <param name="rootObject">The root object.</param>
         protected virtual void OnStart(object rootObject)

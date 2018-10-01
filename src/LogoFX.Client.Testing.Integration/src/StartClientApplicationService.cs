d693e403f0bdeb9e7f7b8a5b74584400900a3bc0
Gennady Verdel <godrose@gmail.com>
2018-10-01 09:02:36 +0300

    Added start client application service

---
+++ src/LogoFX.Client.Testing.Integration/src/StartClientApplicationService.cs
@@ -0,0 +1,22 @@
+﻿using LogoFX.Client.Testing.Contracts;
+
+namespace LogoFX.Client.Testing.Integration
+{
+    /// <inheritdoc />
+    public sealed class StartClientApplicationService : IStartClientApplicationService
+    {
+        private readonly IStartApplicationService _startApplicationService;
+
+        /// <inheritdoc />
+        public StartClientApplicationService(IStartApplicationService startApplicationService)
+        {
+            _startApplicationService = startApplicationService;
+        }
+
+        /// <inheritdoc />
+        public void StartApplication()
+        {
+            _startApplicationService.StartApplication(string.Empty);
+        }
+    }
+}
